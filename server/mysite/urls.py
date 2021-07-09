7f519e0cd897858d26aceee491f42e202e37a1d9
Yordy2001 <kingyordi2001@gmail.com>
2021-07-09 10:14:49 -0400

    update views

--- server/mysite/urls.py
+++ server/mysite/urls.py
@@ -14,10 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 # from django.mysite.polls.views import index
+
 from django.contrib import admin
 from django.urls import path, include
+from  polls.views import IndexView
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index'),
     path('admin/', admin.site.urls),
     path('polls/', include('polls.urls'))
 ]
