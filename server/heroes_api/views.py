2f471f15571008d5d405077ad94ed2f99fa5ade6
Yordy2001 <kingyordi2001@gmail.com>
2022-05-03 22:00:56 -0400

    fix get route

--- server/heroes_api/views.py
+++ server/heroes_api/views.py
@@ -1,7 +1,12 @@
-from http.client import HTTPResponse
+import imp
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
-# Create your views here.
+from django.http import JsonResponse
 
+from .models import Hero
+
+# Create your views here.
 def index(request):
-    return HTTPResponse("Hello, Word!")
+    print(Hero.objects.all())
+
+    return JsonResponse({"data":list(Hero.objects.all())})
