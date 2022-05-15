0398d1824efadaaedb21b25fa265cc76cefba09b
Yordy2001 <kingyordi2001@gmail.com>
2022-05-15 11:28:58 -0400

    create user serializer

--- server/heroes_api/serializer.py
+++ server/heroes_api/serializer.py
@@ -1,7 +1,10 @@
 from dataclasses import fields
+from pydoc import importfile
 from pyexpat import model
 from rest_framework  import serializers
+
 from .models.heroes_stats import Hero, Company, Power_stats
+from .models.user import User
 
 class HeroSerializer(serializers.ModelSerializer):
     class Meta:
@@ -17,3 +20,8 @@ class PowerStatsSerializer(serializers.ModelSerializer):
     class Meta:
         model = Power_stats
         fields = [ "hero", "intelligence", "strength", "speed", "durability", "power", "combat" ]
+
+class HeroSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Hero
+        fields = [ "user_name", "email", "password", "permission"]
